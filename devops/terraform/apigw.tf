223e1f470976fb7df676af5a018ce6c0062b57f2
Luiz - Personal <luizgmaker@gmail.com>
2025-02-09 22:53:03 -0300

    fixed envs

--- devops/terraform/apigw.tf
+++ devops/terraform/apigw.tf
@@ -20,7 +20,7 @@ resource "aws_api_gateway_method" "proxy" {
 
   request_parameters = {
     "method.request.path.proxy"           = true
-    "method.request.header.Authorization" = false
+    # "method.request.header.Authorization" = false
   }
 }
 
@@ -38,10 +38,12 @@ resource "aws_api_gateway_integration" "proxy" {
   request_parameters = {
     "integration.request.path.proxy"           = "method.request.path.proxy"
     "integration.request.header.Accept"        = "'application/json'"
-    "integration.request.header.Authorization" = "method.request.header.Authorization"
+    # "integration.request.header.Authorization" = "method.request.header.Authorization"
   }
 
   connection_type = "VPC_LINK"
   connection_id   = aws_api_gateway_vpc_link.bff.id
+
+  depends_on = [ aws_api_gateway_method.proxy ]
 }
 #endregion
